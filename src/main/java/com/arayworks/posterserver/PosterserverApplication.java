923fe0cdff5a010dc7708511d383c2011e884a1a
Brian Arayathel <brian@brians-air.myfiosgateway.com>
2023-03-04 16:49:13 -0500

    first init

---
+++ src/main/java/com/arayworks/posterserver/PosterserverApplication.java
@@ -0,0 +1,13 @@
+package com.arayworks.posterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosterserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosterserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arayworks/posterserver/PosterserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arayworks.posterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosterserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
